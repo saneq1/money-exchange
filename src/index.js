40843b8a686239e757d2d0c14bd65cdcb4de0ffb
Alexandr Kastukov <thesaneq@gmail.com>
2019-02-27 19:36:26 +0300

    taskmaney

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const H = 50,
+    Q = 25,
+    D = 10,
+    N = 5,
+    P = 1;
+const obj = {};
+
+
+if(currency>10000){ obj['error'] = "You are rich, my friend! We don't have so much coins for exchange";
+return obj;
+}
+if (currency >= H) {
+    var countH = Math.floor(currency / H);
+    currency = currency % H;
+};
+if (currency >= Q) {
+    var countQ = Math.floor(currency / Q);
+    currency = currency % Q;
+};
+if (currency >= D) {
+    var countD = Math.floor(currency / D);
+    currency = currency % D;  
+};
+if (currency >= N) {
+    var countN = Math.floor(currency / N);
+    currency = currency % N;
+};
+if (currency >= P) {
+    var countP = currency / P;
+    currency = currency % P;
+};
+
+if (!!countH) {
+    obj['H'] = countH;
+}if (!!countQ) {
+    obj['Q'] = countQ;
+}if (!!countD) {
+    obj['D'] = countD;
+}if (!!countN) {
+    obj['N'] = countN;
+}if (!!countP) {
+    obj['P'] = countP;
+}
+return obj;
+ 
 }
